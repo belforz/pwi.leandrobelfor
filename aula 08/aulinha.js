23928266eb8828e537dde34fd1a343313b6e01b0
belforz <macedobeiramar@hotmail.com>
2022-10-26 22:15:02 -0300

    AULA DE JAVASCRIPT EDITAR DEPOIS

---
+++ aula 08/aulinha.js
@@ -0,0 +1,19 @@
+console.log("ola!!!")
+/* console.log é um comando que vai exibir
+ informações no console do navegador. */
+
+ var Variavel1 = 2;
+ // var tem o tipo de escopo global
+ let Variavel2 = "O";
+ // let tem o tipo de escopo local ;  let é um função que somente existirá dentro de um escopo específico ; pode ser reescrita
+ const Variavel3 = 3 + 3;
+ // const é de escopo global e não pode ser reescrita
+
+ console.log("A variável Variavel1 é igual a ", Variavel1)
+ Variavel1 = 4;
+ console.log("Agora variável Variavel1 é igual ", Variavel1)
+ console.log ("O valor de Variavel2 é igual a ", Variavel2)
+ Variavel2 =  'eu quero p(/a)'
+ console.log("Agora o valor de Variavel2 é ", Variavel2)
+ console.log("Esssa é a contante ", Variavel3)
+ Variavel3 = 9
