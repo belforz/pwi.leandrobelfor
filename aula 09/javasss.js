30dab42a6219b3be427abcace11850fac691a022
belforz <macaedobeiramar@hotmail.com>
2022-11-09 22:23:36 -0300

    evolução de java

---
+++ aula 09/javasss.js
@@ -0,0 +1,9 @@
+const lol = document.getElementById("lol")
+const dois = document.getElementById("dois")
+
+let text = "Leandro Costa Belfor"
+let part = text.slice(-6)
+let tamanho = text.length
+document.getElementById("lol").innerHTML = part ;
+document.getElementById("dois").innerHTML = tamanho ;
+       
