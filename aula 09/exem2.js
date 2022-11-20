2798c613a2887a72cbd234481b522ecd5b3b2e75
belforz <macedobeiramar@hotmail.com>
2022-11-20 00:29:12 -0300

    mudanças na pasta 09

---
+++ aula 09/exem2.js
@@ -0,0 +1,9 @@
+function curioso() {
+    document.getElementById("demo1").innerHTML = "Como cu de curioso";
+}
+
+let img = document.createElement("img");
+    img.src = "homem.png";
+
+var div = document.getElementById("holder");
+div.appendChild(img);   
